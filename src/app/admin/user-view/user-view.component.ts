4cb7e48260d479616b675e3827df46b22f2b6393
Atalaa <atala.rchidi@gmail.com>
2022-09-07 21:52:43 -0400

    display route parameter on click

--- src/app/admin/user-view/user-view.component.ts
+++ src/app/admin/user-view/user-view.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { map, switchMap } from 'rxjs/operators';
+import { User } from '../../services/user.model';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-view',
@@ -10,19 +13,11 @@ export class UserViewComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
 
   id: string;
- 
+
   ngOnInit() {
-    this.route.paramMap.subscribe(e => {
-      console.log(e.get('id'))
-    })
-  };
-
-
-    // .pipe(
-    //   map(el => el.get('id')),
-    //   switchMap()
-    // )
-
+    this.route.paramMap.subscribe((e) => {
+      this.id = e.get('id');
+    });
     console.log(this.route.snapshot.paramMap.get('id'));
   }
 }
