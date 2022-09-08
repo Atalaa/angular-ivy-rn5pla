29f0e7aa001da597a63be4d6ec64b9ed602eb6c1
Atalaa <atala.rchidi@gmail.com>
2022-09-07 21:04:58 -0400

    continue working on routes

--- src/app/admin/user-view/user-view.component.ts
+++ src/app/admin/user-view/user-view.component.ts
@@ -4,14 +4,25 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-user-view',
   templateUrl: './user-view.component.html',
-  styleUrls: ['./user-view.component.css']
+  styleUrls: ['./user-view.component.css'],
 })
-export class   implements OnInit {
+export class UserViewComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
 
-  constructor(private route: ActivatedRoute) { }
-
+  id: string;
+ 
   ngOnInit() {
-    console.log(this.route.snapshot.paramMap.get('id'))
-  }
+    this.route.paramMap.subscribe(e => {
+      console.log(e.get('id'))
+    })
+  };
 
+
+    // .pipe(
+    //   map(el => el.get('id')),
+    //   switchMap()
+    // )
+
+    console.log(this.route.snapshot.paramMap.get('id'));
+  }
 }
